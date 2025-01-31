2c5d735f01b876811fa404a4156d6e5f8bac313c
KunalSood-19 <kunaalsood15@gmail.com>
2025-01-31 11:51:54 +0530

    Solution to Check MSB of a Number

---
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    r=n&(1<<31);
+    printf("%d",r);
+    return 0;
+}
